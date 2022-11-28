6c4a7979c7a4d4299993651f127c18035ba5cbc2
janika.kalliokoski@helsinki.fi <jbkallio@cubbli20-gold.local>
2022-11-28 13:10:18 +0200

    kaksi eri tuotetta korissa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -38,4 +38,10 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(self.kori.hinta(), 5)
 
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_tavaroiden_maara_kaksi(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
 
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
